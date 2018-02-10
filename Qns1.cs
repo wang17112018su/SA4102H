27dd485128a932bb2e8ca88d76e20f047fa5feac
wang17112018su <36073580+wang17112018su@users.noreply.github.com>
2018-02-10 15:08:20 +0800

    Add files via upload

---
+++ Qns1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionH
+{
+    class Qns1
+    
+    {
+       
+        public static int ReadInteger(string message)
+        {
+            bool IsParseDone;
+            int userinput;
+            do
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+                IsParseDone = Int32.TryParse(input, out userinput);
+            }
+            while (IsParseDone == false);
+           
+
+            return userinput;
+        }
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionH
+{
+    class Start
+    {
+        static void Main()
+        {
+            int x = Qns1.ReadInteger("Hi enter a no.");
+            Console.WriteLine("The number you wrote is {0}", x);
+        }
+    }
+}
